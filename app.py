5ab4795d98112ad4ba683c6d348ca90d6636e3c3
jseoanesleon <jairo.seoanes@unilibre.edu.co>
2025-05-19 13:28:34 -0500

    Talento Tech Legend

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>¡Bienvenido al curso de Docker con Python!</h1>"
+    return "<h1>¡Introduccion a Docker con AWS - Talento Tech Caribe!</h1>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
